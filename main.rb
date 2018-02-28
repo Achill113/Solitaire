12065f9f1820e7514afb7c7ae2d0cd4afc45e5a2
Alex <alex.hill@trinitysolarsystems.com>
2018-02-28 16:59:38 -0500

    Refactoring

--- main.rb
+++ main.rb
@@ -1,13 +1,15 @@
 require 'gosu'
 
 class Solitaire < Gosu::Window
+
+  attr_accessor :deck
+
   def initialize
     super 800, 500
     self.caption = "Solitaire"
 
     @background_image = Gosu::Image.new("media/table.jpg", :tileable => false)
-    build_deck
-    setup
+    @deck = Deck.new
   end
 
   def update
@@ -15,7 +17,7 @@ class Solitaire < Gosu::Window
 
   def draw
     @background_image.draw(0,0,0)
-    $stacks.each{|stack|
+    @deck.stacks.each{|stack|
       stack.each{|card|
         card.face.draw(card.x,card.y,card.z,scale_x=1,scale_y=1)
       }
@@ -24,14 +26,6 @@ class Solitaire < Gosu::Window
 
   def needs_cursor?
     true
-  end
-
-  def build_deck
-    d = Deck.new
-    @deck = []
-    d.cards.each{|card|
-      @deck << card
-    }
   end
 end
 
@@ -62,71 +56,70 @@ class Card
 end
 
 class Deck
-  attr_accessor :cards
+  attr_accessor :cards, :deck, :stacks
 
   def initialize
-    self.cards = (0..51).to_a.shuffle.collect{|id|
+    @cards = (0..51).to_a.shuffle.collect{|id|
       Card.new(id)
     }
-  end
-end
-
-def build_deck
-  d = Deck.new
-  @deck = []
-  d.cards.each{|card|
-    @deck << card
-  }
-end
-
-# build_deck
-
-def setup
-  $stacks = []
-  $next_card = []
-  $card_back = Gosu::Image.new("media/card_back.png", options = {:rect => [0,0,5,10]})#, tileable => false)
-
-  x = 1
-  for i in 0..6
-    $stacks[i] = move_from_deck $stacks[i], x
-    x += 1
+    build_deck
+    setup
   end
 
-  $a1 = []
-  $a2 = []
-  $a3 = []
-  $a4 = []
+  public
 
-  $stacks.each{|stack|
-    stack.last.visible = true
-  }
-  x = 20
-  y = 100
-  $stacks.each{|stack|
-    z = 0
-    stack.each{|card|
-      if card.visible
-        card.x = x
-        card.y = y
-        card.z = z
-      else
-        card.face = $card_back
-        card.x = x
-        card.y = y
-        card.z = z
-      end
-      z += 1
+  def build_deck
+    @deck = []
+    @cards.each{|card|
+      @deck << card
     }
-    x += 50
-  }
-end
+  end
 
-def move_from_deck destination, x
-  destination = @deck.last(x)
-  @deck.pop(x)
-end
+  def setup
+    @stacks = []
+    $next_card = []
+    $card_back = Gosu::Image.new("media/card_back.png", options = {:rect => [0,0,5,10]})#, tileable => false)
 
-# setup
+    x = 1
+    for i in 0..6
+      @stacks[i] = move_from_deck @stacks[i], x
+      x += 1
+    end
+
+    $a1 = []
+    $a2 = []
+    $a3 = []
+    $a4 = []
+
+    @stacks.each{|stack|
+      stack.last.visible = true
+    }
+    x = 20
+    y = 100
+    @stacks.each{|stack|
+      z = 0
+      stack.each{|card|
+        if card.visible
+          card.x = x
+          card.y = y
+          card.z = z
+        else
+          card.face = $card_back
+          card.x = x
+          card.y = y
+          card.z = z
+        end
+        z += 1
+      }
+      x += 50
+    }
+  end
+
+  def move_from_deck destination, x
+    destination = @deck.last(x)
+    @deck.pop(x)
+  end
+end
 
 def move stack1, stack2, num
   if stack1.last.visible && stack2.last.visible && stack1.last.rank == (stack2.last.rank + 1)
@@ -138,6 +131,6 @@ def move stack1, stack2, num
   end
 end
 
-# move $stacks[0], $stacks[3], 1
-# puts $stacks
-Solitaire.new.show
+GUI = Thread.new{
+  $game = Solitaire.new
+}
